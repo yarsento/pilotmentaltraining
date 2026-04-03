b4f15dfa712c974742ff4db570a2f1e61e2bfbd9
Yannik Hupfer <yannikhupfer@gmail.com>
2026-04-03 13:25:42 +0200

    Updated base path in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,5 @@ export default defineConfig(({ mode }) => ({
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
+  base: '/pilotmentaltraining/',
 }));
