7f0fbbae71ad3700c82e011b79bcdcdab4c1acce
Yannik Hupfer <yannikhupfer@gmail.com>
2026-04-03 13:23:16 +0200

    Updated basename

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/pilotmentaltraining">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/module/:id" element={<ModulePage />} />
